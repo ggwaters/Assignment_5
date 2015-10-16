9ecdf455e955bfe640494e64264449275b631f6a
ggwaters <the_inbox@att.net>
2015-10-15 17:51:04 -0700

    new upload

---
+++ calculator/scripts.js
@@ -0,0 +1,19 @@
+//document.addEventListener("DOMContentLoad", init, false);
+//
+//function init() {
+//    seven.addEventListener("click", enter, false);
+//    eight.addEventListener("click", enter, false);
+//    nine.addEventListener("click", enter, false);
+//    divide.addEventListener("click", enter, false);
+//    four.addEventListener("click", enter, false);
+//    five.addEventListener("click", enter, false);
+//    six.addEventListener("click", enter, false);
+//    multiply.addEventListener("click", enter, false);
+//    one.addEventListener("click", enter, false);
+//    two.addEventListener("click", enter, false);
+//    three.addEventListener("click", enter, false);
+//    subtract.addEventListener("click", enter, false);
+//    zero.addEventListener("click", enter, false);
+//    period.addEventListener("click", enter, false);
+//    add.addEventListener("click", enter, false);
+//    ;
